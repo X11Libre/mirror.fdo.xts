X11Libre/mirror.fdo.xts

commit 1d198dac664ca4429865acf48d37ac2882b528ae

Fix -Wmissing-prototypes warnings in xts5 framework
Gets rid of 411 -Wmissing-prototypes warnings from gcc 14

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/test/xts/-/merge_requests/10>

xts5/src/libXtTest/AvsComp2.c
@@ -76,13 +76,15 @@ SOFTWARE.
 #include "AvsCompP.h"
 
 					/* widget class method */
-static void             ClassInitialize();
-static void             Initialize();
-static void             Destroy();
-static Boolean          SetValues();
-static XtGeometryResult GeometryManager();
-static XtGeometryResult	QueryGeometry();
-static void             ChangeManagedTest();
+static void             ClassInitialize(void);
+static void             Initialize(Widget, Widget, ArgList, Cardinal *);
+static void             Destroy(Widget);
+static Boolean          SetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static XtGeometryResult GeometryManager(Widget, XtWidgetGeometry *,
+                                        XtWidgetGeometry *);
+static XtGeometryResult	QueryGeometry(Widget, XtWidgetGeometry *,
+                                      XtWidgetGeometry *);
+static void             ChangeManagedTest(Widget);
 
 
 #ifdef ADD_RESOURCES
@@ -177,7 +179,7 @@ Widget w;
 {
 	avs_set_event(5,1);
 }
-static void ClassInitialize ()
+static void ClassInitialize (void)
 {
     XawInitializeWidgetSet();
 }

xts5/src/libXtTest/AvsForm.c
@@ -122,12 +122,19 @@ static XtResource formConstraintResources[] = {
 };
 #undef Offset
 
-static void ClassInitialize(), ClassPartInitialize(), Initialize(), Resize();
-static void ConstraintInitialize();
-static Boolean SetValues(), ConstraintSetValues();
-static XtGeometryResult GeometryManager(), PreferredGeometry();
-static void ChangeManaged();
-static Boolean Layout();
+static void ClassInitialize(void);
+static void ClassPartInitialize(WidgetClass);
+static void Initialize(Widget, Widget, ArgList, Cardinal *);
+static void Resize(Widget);
+static void ConstraintInitialize(Widget, Widget, ArgList, Cardinal *);
+static Boolean SetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static Boolean ConstraintSetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static XtGeometryResult GeometryManager(Widget, XtWidgetGeometry *,
+                                        XtWidgetGeometry *);
+static XtGeometryResult PreferredGeometry(Widget, XtWidgetGeometry *,
+                                          XtWidgetGeometry *);
+static void ChangeManaged(Widget);
+static Boolean Layout(FormWidget, unsigned int, unsigned int, Bool);
 
 static void
 GetValuesHook(w, args, num_args)
@@ -258,7 +265,7 @@ static void _CvtStringToEdgeType(args, num_args, fromVal, toVal)
     toVal->size = 0;
 }
 
-static void ClassInitialize()
+static void ClassInitialize(void)
 {
     static XtConvertArgRec parentCvtArgs[] = {
 	{XtBaseOffset, (char *)XtOffset(Widget, core.parent), sizeof(Widget)}
@@ -285,8 +292,8 @@ static void ClassPartInitialize(class)
 }
 
 /* ARGSUSED */
-static void Initialize(request, new)
-    Widget request, new;
+static void Initialize(Widget request, Widget new,
+                       ArgList args, Cardinal *num_args)
 {
     FormWidget fw = (FormWidget)new;
 
@@ -710,16 +717,16 @@ static XtGeometryResult GeometryManager(w, request, reply)
 
 
 /* ARGSUSED */
-static Boolean SetValues(current, request, new)
-    Widget current, request, new;
+static Boolean SetValues(Widget current, Widget request, Widget new,
+                         ArgList args, Cardinal *num_args)
 {
     return( FALSE );
 }
 
 
 /* ARGSUSED */
-static void ConstraintInitialize(request, new)
-    Widget request, new;
+static void ConstraintInitialize(Widget request, Widget new,
+                                 ArgList args, Cardinal *num_args)
 {
     FormConstraints form = (FormConstraints)new->core.constraints;
     FormWidget fw = (FormWidget)new->core.parent;
@@ -735,8 +742,8 @@ static void ConstraintInitialize(request, new)
 }
 
 /* ARGSUSED */
-static Boolean ConstraintSetValues(current, request, new)
-    Widget current, request, new;
+static Boolean ConstraintSetValues(Widget current, Widget request, Widget new,
+                                   ArgList args, Cardinal *num_args)
 {
 	avs_set_event(4,1);
     return( TRUE );

xts5/src/libXtTest/AvsForm2.c
@@ -108,12 +108,19 @@ static XtResource formConstraintResources[] = {
 };
 #undef Offset
 
-static void ClassInitialize(), ClassPartInitialize(), Initialize(), Resize();
-static void ConstraintInitialize();
-static Boolean SetValues(), ConstraintSetValues();
-static XtGeometryResult GeometryManager(), PreferredGeometry();
-static void ChangeManaged();
-static Boolean Layout();
+static void ClassInitialize(void);
+static void ClassPartInitialize(WidgetClass);
+static void Initialize(Widget, Widget, ArgList, Cardinal *);
+static void Resize(Widget);
+static void ConstraintInitialize(Widget, Widget, ArgList, Cardinal *);
+static Boolean SetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static Boolean ConstraintSetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static XtGeometryResult GeometryManager(Widget, XtWidgetGeometry *,
+                                        XtWidgetGeometry *);
+static XtGeometryResult PreferredGeometry(Widget, XtWidgetGeometry *,
+                                          XtWidgetGeometry *);
+static void ChangeManaged(Widget);
+static Boolean Layout(FormWidget, unsigned int, unsigned int, Bool);
 
 FormClassRec avsform2ClassRec = {
   { /* core_class fields */
@@ -227,7 +234,7 @@ static void _CvtStringToEdgeType(args, num_args, fromVal, toVal)
     toVal->size = 0;
 }
 
-static void ClassInitialize()
+static void ClassInitialize(void)
 {
     static XtConvertArgRec parentCvtArgs[] = {
 	{XtBaseOffset, (XtPointer)XtOffset(Widget, core.parent), sizeof(Widget)}
@@ -254,8 +261,8 @@ static void ClassPartInitialize(class)
 }
 
 /* ARGSUSED */
-static void Initialize(request, new)
-    Widget request, new;
+static void Initialize(Widget request, Widget new,
+                       ArgList args, Cardinal *num_args)
 {
     FormWidget fw = (FormWidget)new;
 
@@ -665,8 +672,8 @@ static XtGeometryResult GeometryManager(w, request, reply)
 
 
 /* ARGSUSED */
-static Boolean SetValues(current, request, new)
-    Widget current, request, new;
+static Boolean SetValues(Widget current, Widget request, Widget new,
+                         ArgList args, Cardinal *num_args)
 {
     return( FALSE );
 }

xts5/src/libXtTest/AvsForm3.c
@@ -100,12 +100,19 @@ static XtResource formConstraintResources[] = {
 };
 #undef Offset
 
-static void ClassInitialize(), ClassPartInitialize(), Initialize(), Resize();
-static void ConstraintInitialize();
-static Boolean SetValues(), ConstraintSetValues();
-static XtGeometryResult GeometryManager(), PreferredGeometry();
-static void ChangeManaged();
-static Boolean Layout();
+static void ClassInitialize(void);
+static void ClassPartInitialize(WidgetClass);
+static void Initialize(Widget, Widget, ArgList, Cardinal *);
+static void Resize(Widget);
+static void ConstraintInitialize(Widget, Widget, ArgList, Cardinal *);
+static Boolean SetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static Boolean ConstraintSetValues(Widget, Widget, Widget, ArgList, Cardinal *);
+static XtGeometryResult GeometryManager(Widget, XtWidgetGeometry *,
+                                        XtWidgetGeometry *);
+static XtGeometryResult PreferredGeometry(Widget, XtWidgetGeometry *,
+                                          XtWidgetGeometry *);
+static void ChangeManaged(Widget);
+static Boolean Layout(FormWidget, unsigned int, unsigned int, Bool);
 
 FormClassRec avsform3ClassRec = {
   { /* core_class fields */
@@ -219,7 +226,7 @@ static void _CvtStringToEdgeType(args, num_args, fromVal, toVal)
     toVal->size = 0;
 }
 
-static void ClassInitialize()
+static void ClassInitialize(void)
 {
     static XtConvertArgRec parentCvtArgs[] = {
 	{XtBaseOffset, (XtPointer)XtOffset(Widget, core.parent), sizeof(Widget)}
@@ -246,8 +253,8 @@ static void ClassPartInitialize(class)
 }
 
 /* ARGSUSED */
-static void Initialize(request, new)
-    Widget request, new;
+static void Initialize(Widget request, Widget new,
+                       ArgList args, Cardinal *num_args)
 {
     FormWidget fw = (FormWidget)new;
 
@@ -657,8 +664,8 @@ static XtGeometryResult GeometryManager(w, request, reply)
 
 
 /* ARGSUSED */
-static Boolean SetValues(current, request, new)
-    Widget current, request, new;
+static Boolean SetValues(Widget current, Widget request, Widget new,
+                         ArgList args, Cardinal *num_args)
 {
     return( FALSE );
 }

commit 12a887c2c72c4258962b56ced7b0aec782f1ffed

libXtTest: Make function declarations match their prototypes
Fixes build errors when compiling in Xserver CI.
Not sure why this wasn't flagged locally or in XTS CI.

Fixes: 1d198dac ("Fix -Wmissing-prototypes warnings in xts5 framework")
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/test/xts/-/merge_requests/12>

xts5/src/libXtTest/AvsComp2.c
@@ -185,8 +185,8 @@ static void ClassInitialize (void)
 }
 
 
-static void Initialize (grequest, gnew)
-    Widget grequest, gnew;
+static void Initialize (Widget grequest, Widget gnew,
+                        ArgList args, Cardinal *num_args)
 {
     AvsComp2Widget request = (AvsComp2Widget) grequest, new = (AvsComp2Widget) gnew;
 
@@ -200,8 +200,8 @@ static void Initialize (grequest, gnew)
 
 
 /* ARGSUSED */
-static Boolean SetValues (gcurrent, grequest, gnew)
-    Widget gcurrent, grequest, gnew;
+static Boolean SetValues (Widget grequest, Widget gnew, Widget gcurrent,
+                          ArgList args, Cardinal *num_args)
 {
 /*
     AvsComp2Widget current = (AvsComp2Widget) gcurrent, new = (AvsComp2Widget) gnew;

xts5/src/libXtTest/AvsForm.c
@@ -465,10 +465,10 @@ Widget w;
  */
 
 /* ARGSUSED */
-static Boolean Layout(fw, width, height, force_relayout)
-    FormWidget fw;
-    Dimension width, height;
-    Boolean force_relayout;
+static Boolean Layout(
+    FormWidget fw,
+    unsigned int width, unsigned int height,
+    Bool force_relayout)
 {
     int num_children = fw->composite.num_children;
     WidgetList children = fw->composite.children;

xts5/src/libXtTest/AvsForm2.c
@@ -435,10 +435,10 @@ Widget w;
  */
 
 /* ARGSUSED */
-static Boolean Layout(fw, width, height, force_relayout)
-    FormWidget fw;
-    Dimension width, height;
-    Boolean force_relayout;
+static Boolean Layout(
+    FormWidget fw,
+    unsigned int width, unsigned int height,
+    Bool force_relayout)
 {
     int num_children = fw->composite.num_children;
     WidgetList children = fw->composite.children;
@@ -680,8 +680,8 @@ static Boolean SetValues(Widget current, Widget request, Widget new,
 
 
 /* ARGSUSED */
-static void ConstraintInitialize(request, new)
-    Widget request, new;
+static void ConstraintInitialize(Widget request, Widget new,
+                                 ArgList args, Cardinal *num_args)
 {
     FormConstraints form = (FormConstraints)new->core.constraints;
     FormWidget fw = (FormWidget)new->core.parent;
@@ -697,8 +697,8 @@ static void ConstraintInitialize(request, new)
 }
 
 /* ARGSUSED */
-static Boolean ConstraintSetValues(current, request, new)
-    Widget current, request, new;
+static Boolean ConstraintSetValues(Widget current, Widget request, Widget new,
+                                   ArgList args, Cardinal *num_args)
 {
 /*avsform's procedure does event 4, allows order checking*/
 	avs_set_event(3, avs_get_event(4)+1);

xts5/src/libXtTest/AvsForm3.c
@@ -427,10 +427,10 @@ Widget w;
  */
 
 /* ARGSUSED */
-static Boolean Layout(fw, width, height, force_relayout)
-    FormWidget fw;
-    Dimension width, height;
-    Boolean force_relayout;
+static Boolean Layout(
+    FormWidget fw,
+    unsigned int width, unsigned int height,
+    Bool force_relayout)
 {
     int num_children = fw->composite.num_children;
     WidgetList children = fw->composite.children;
@@ -672,8 +672,8 @@ static Boolean SetValues(Widget current, Widget request, Widget new,
 
 
 /* ARGSUSED */
-static void ConstraintInitialize(request, new)
-    Widget request, new;
+static void ConstraintInitialize(Widget request, Widget new,
+                                 ArgList args, Cardinal *num_args)
 {
     FormConstraints form = (FormConstraints)new->core.constraints;
     FormWidget fw = (FormWidget)new->core.parent;
@@ -689,8 +689,8 @@ static void ConstraintInitialize(request, new)
 }
 
 /* ARGSUSED */
-static Boolean ConstraintSetValues(current, request, new)
-    Widget current, request, new;
+static Boolean ConstraintSetValues(Widget current, Widget request, Widget new,
+                                   ArgList args, Cardinal *num_args)
 {
     return( FALSE );
 }